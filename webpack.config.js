3a8bf3dac1f39d61f773fb8fa22d31193a0b7642
Artyom Kayun <kayun.artem@gmail.com>
2017-05-23 23:09:50 +0600

    init project

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SpritesmithPlugin = require('webpack-spritesmith');
+
+const rupture = require('rupture')
+
+const SRC_DIR = path.resolve(__dirname, 'src')
+
+let templates = () => {
+  let templatesPath = `${SRC_DIR}/templates/pages`;
+  let files = fs.readdirSync(templatesPath);
+
+  return files.map(file => {
+    return new HtmlWebpackPlugin({
+      filename: `${path.basename(file, '.pug')}.html`,
+      template: `${templatesPath}/${file}`
+    })
+  })
+};
+
+module.exports = {
+  context: SRC_DIR,
+  entry: [
+    './styles/index.styl'
+  ],
+  output: {
+    filename: 'assets/[name].js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: [ '.js', '.styl', '.pug' ],
+    modules: [
+      'node_modules',
+      'spritesmith-generated',
+      path.resolve(__dirname, 'src')
+    ]
+  },
+  target: 'web',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins() {
+                  return [
+                    require('autoprefixer')
+                  ]
+                }
+              }
+            },
+            {
+              loader: 'stylus-loader',
+              options: {
+                use: [rupture()]
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.pug$/,
+        use: {
+          loader: 'pug-loader'
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new CleanWebpackPlugin(['public']),
+    new ManifestPlugin({
+      publicPath: '/public/',
+      fileName: './assets.json',
+      writeToFileEmit: true
+    }),
+    new ExtractTextPlugin('assets/styles/common.css'),
+    new webpack.HotModuleReplacementPlugin(),
+    new SpritesmithPlugin({
+      src: {
+        cwd: path.resolve(__dirname, 'src/images/sprite'),
+        glob: '*.png'
+      },
+      target: {
+        image: path.resolve(__dirname, 'public/assets/images/sprite.png'),
+        css: path.resolve(__dirname, 'src/styles/helpers/sprite.styl')
+      },
+      apiOptions: {
+        cssImageRef: "/assets/images/sprite.png"
+      }
+    })
+  ].concat(templates()),
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 3000,
+    hot: true
+  }
+};
+
