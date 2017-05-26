b48f0e7139f97b7cd7242c7e44670561640c7a4b
Artyom Kayun <kayun.artem@gmail.com>
2017-05-26 20:05:06 +0600

    add date, select answer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,12 @@
+var answers = Array.prototype.slice.apply(document.querySelectorAll('.js-radio'));
 
+document.addEventListener('keypress', function (event) {
+  var char = String.fromCharCode(event.keyCode).toUpperCase();
+  answers.forEach(function (answer) {
+    var data = answer.dataset.letter;
+    if (data === char) {
+      answer.checked = true;
+      return false;
+    }
+  })
+})
