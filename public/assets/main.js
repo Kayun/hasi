e91b8f86961bf9af7eb310b92836f6103baeef51
Artyom Kayun <kayun.artem@gmail.com>
2017-06-01 17:37:37 +0600

    build08

--- public/assets/main.js
+++ public/assets/main.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "1a45288ee2d4164989c8"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "bb03d802138de64b2118"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
@@ -1351,7 +1351,7 @@ module.exports = shouldUseNative() ? Object.assign : function (target, source) {
 
 // removed by extract-text-webpack-plugin
     if(true) {
-      // 1496316238806
+      // 1496317021201
       const cssReload = __webpack_require__(4)({"fileMap":"{fileName}"});
       module.hot.dispose(cssReload);
       module.hot.accept(undefined, cssReload);
