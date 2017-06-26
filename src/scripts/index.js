23c8adf8c0b7f97ee2b5306f2128915f4c913435
Artyom Kayun <kayun.artem@gmail.com>
2017-06-26 15:40:39 +0600

    v0.0.2-build08

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,69 +1,9 @@
 import { Calculator } from './calculator';
 import $ from 'jquery';
 import 'jquery-ui/ui/widgets/sortable';
-
+import 'imports-loader?jQuery=jquery!jquery-ui-touch-punch';
 
 $(() => {
-
-  (function( $ ) {
-
-    $.support.touch = typeof Touch === 'object';
-
-    if (!$.support.touch) {
-      return;
-    }
-
-    var proto =  $.ui.mouse.prototype,
-      _mouseInit = proto._mouseInit;
-
-    $.extend( proto, {
-      _mouseInit: function() {
-        this.element
-          .bind( "touchstart." + this.widgetName, $.proxy( this, "_touchStart" ) );
-        _mouseInit.apply( this, arguments );
-      },
-
-      _touchStart: function( event ) {
-        if ( event.originalEvent.targetTouches.length != 1 ) {
-          return false;
-        }
-
-        this.element
-          .bind( "touchmove." + this.widgetName, $.proxy( this, "_touchMove" ) )
-          .bind( "touchend." + this.widgetName, $.proxy( this, "_touchEnd" ) );
-
-        this._modifyEvent( event );
-
-        $( document ).trigger($.Event("mouseup")); //reset mouseHandled flag in ui.mouse
-        this._mouseDown( event );
-
-        return false;
-      },
-
-      _touchMove: function( event ) {
-        this._modifyEvent( event );
-        this._mouseMove( event );
-      },
-
-      _touchEnd: function( event ) {
-        this.element
-          .unbind( "touchmove." + this.widgetName )
-          .unbind( "touchend." + this.widgetName );
-        this._mouseUp( event );
-      },
-
-      _modifyEvent: function( event ) {
-        event.which = 1;
-        var target = event.originalEvent.targetTouches[0];
-        event.pageX = target.clientX;
-        event.pageY = target.clientY;
-      }
-
-    });
-
-  })($);
-
-
   let $fontSwitch = $('.js-font-switch');
   let $font = $('.js-font');
 
@@ -97,7 +37,6 @@ $(() => {
   $sortContainer.length && $sortContainer.sortable({
     stop: (event, ui) => {
       let $counts = $('.js-sort-count', $sortContainer);
-      console.log($counts);
       $counts.each((index, item) => $(item).text(index + 1));
     }
   });
