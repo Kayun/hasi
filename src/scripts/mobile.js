8a2616a74cec85304f0b60c4c223528b0bc818ce
Artyom Kayun <kayun.artem@gmail.com>
2017-09-01 21:08:13 +0600

    v0.0.3-build08

--- src/scripts/mobile.js
+++ src/scripts/mobile.js
@@ -2,10 +2,13 @@ import $ from 'jquery';
 
 $(() => {
   let $toggles = $('.js-toggle');
+  let $togglesOption = $('');
 
   $toggles.on('touchstart', function(event) {
     event.stopPropagation();
 
-    $(this).toggleClass('_open');
+    $(this)
+      .toggleClass('_open')
+      .next('.js-toggle-container').toggle();
   })
 })
